746032dbf3f8fe0085237f2fe357e5de20ac1f9e
Clint Pitzak <cpitzak@gmail.com>
2016-07-15 20:24:05 -0700

    Add humidity chart and upgrade angular-chartjs version

--- app/controllers/mainController.js
+++ app/controllers/mainController.js
@@ -30,12 +30,15 @@ weatherWebApp.controller('MainController', ['$scope', '$resource', '$interval',
             $resource('/roomWeather/hourly', {}, {query: {method: 'get', isArray: true}}).query(function(obj2) {
                 var i, hourlyData = obj,
                     roomHourlyData = obj2,
-                    roomDataPoints = [],
-                    dataPoints = [],
+                    roomTempDataPoints = [],
+                    roomHumidityDataPoints = [],
+                    outsideTempPoints = [],
+                    outsideHumidityPoints = [],
                     roomWeather = {},
                     roomHourKey;
                 $scope.labels = [];
                 $scope.data = [];
+                $scope.humidityChartData = [];
                 for(i = 0; i < roomHourlyData.length; i++) {
                     roomHourKey = convert24to12Hour(roomHourlyData[i].hour);
                     roomWeather[roomHourKey] =  {};
@@ -45,29 +48,37 @@ weatherWebApp.controller('MainController', ['$scope', '$resource', '$interval',
                 // $scope.labels = ["5:00 AM", "6:00 AM", ...];
                 for(i = 0; i < hourlyData.length; i++) {
                     $scope.labels.push(hourlyData[i].time);
-                    dataPoints.push(hourlyData[i].temp);
+                    outsideTempPoints.push(hourlyData[i].temp);
+                    outsideHumidityPoints.push(hourlyData[i].humidity);
                     if (roomWeather[hourlyData[i].time]) {
-                        roomDataPoints.push(roomWeather[hourlyData[i].time].temp);
+                        roomTempDataPoints.push(roomWeather[hourlyData[i].time].temp);
+                        roomHumidityDataPoints.push(roomWeather[hourlyData[i].time].humidity);
                     } else {
-                        roomDataPoints.push(null);
+                        roomTempDataPoints.push(null);
+                        roomHumidityDataPoints.push(null);
                     }
                 }
                 // $scope.data = [[65, 59, 80, 81, 56, 55, 40]];
-                $scope.data.push(dataPoints);
-                $scope.data.push(roomDataPoints);
+                $scope.data.push(outsideTempPoints);
+                $scope.data.push(roomTempDataPoints);
+                $scope.humidityChartData.push(outsideHumidityPoints);
+                $scope.humidityChartData.push(roomHumidityDataPoints);
                 $scope.series = ['Palo Alto', 'Room'];
                 $scope.onClick = function (points, evt) {
                     console.log(points, evt);
                 };
               $scope.datasetOverride = [{ yAxisID: 'y-axis-1' }];
               $scope.options = {
+                legend: {
+                    display: true
+                },
                 scales: {
                   yAxes: [
                     {
                       id: 'y-axis-1',
                       type: 'linear',
                       display: true,
-                      position: 'left'
+                      position: 'right'
                     }
                   ]
                 }
